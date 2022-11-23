b2d3fcdf34b9376b95d1c23ffd075ebc2b7bec15
Aditya <63107098+Aditya0721@users.noreply.github.com>
2022-11-23 12:19:52 +0530

    created models

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const userModel = require("./models/user.model")
 
 const app = express()
 
 app.listen(8000,()=>{
     console.log("server started")
 })
+
+mongoose.connect("mongodb://localhost/Eshop")
+.then(()=>{
+    console.log("DataBase connected")
+})
+.catch(()=>console.log("Error Occured"))
